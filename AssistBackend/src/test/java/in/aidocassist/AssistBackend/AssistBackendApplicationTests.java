9663be7edb7a5fe86204b1a8935a8392e7b4f26a
Rajtilak Pal <rajtilakls2510@gmail.com>
2021-10-29 19:40:29 +0530

    Added Backend and Frontend Initial Apps

---
+++ AssistBackend/src/test/java/in/aidocassist/AssistBackend/AssistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.aidocassist.AssistBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
