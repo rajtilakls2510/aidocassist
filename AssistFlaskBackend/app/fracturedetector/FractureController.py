5dbfc8f47f9cef7fc61adfeb757ef08c0f4f8e19
Rajtilak Pal <rajtilakls2510@gmail.com>
2022-04-30 12:40:16 +0530

    Replaced Hand Fracture Detector model with bndbox center

--- AssistFlaskBackend/app/fracturedetector/FractureController.py
+++ AssistFlaskBackend/app/fracturedetector/FractureController.py
@@ -10,11 +10,16 @@ REQUESTMAPPING = "/fracture"
 def fracture_detector():
     img = request.files['img']
     img = cv2.imdecode(np.fromstring(img.read(), np.uint8), cv2.IMREAD_COLOR)
-    xmin, ymin, xmax, ymax = model(tf.cast(tf.constant([img]), dtype=tf.float32)).numpy()[0]
+    center_x, center_y, b_width, b_height = model(tf.cast(tf.constant([img]), dtype=tf.float32)).numpy()[0]
     imwidth, imheight = img.shape[1], img.shape[0]
-    xmin, ymin, xmax, ymax = int(xmin * imwidth / 10), int(
-        ymin * imheight / 10), int(xmax * imwidth / 10), int(
-        ymax * imheight / 10)
+    xmin, ymin, xmax, ymax = get_bndbox(imwidth, imheight, center_x, center_y, b_width, b_height)
     img = cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
     img_encoded = base64.b64encode(cv2.imencode('.jpg', img)[1]).decode()
     return jsonify({"detected": img_encoded})
+
+def get_bndbox(im_width, im_height, center_x, center_y, b_width, b_height):
+  xmin = int(center_x*im_width-b_width*im_width/2)
+  xmax = int(center_x*im_width+b_width*im_width/2)
+  ymin = int(center_y*im_height-b_height*im_height/2)
+  ymax = int(center_y*im_height+b_height*im_height/2)
+  return xmin, ymin, xmax, ymax
